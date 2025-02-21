dbf4497fa160f9c4c9e3029f658cfe71ee1e066b
hadesds <hadessds@gmail.com>
2025-02-21 11:38:16 -0300

    Atividade Operadores Unarios

---
+++ src/operadoresUnarios.java
@@ -0,0 +1,28 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class operadoresUnarios {
+    public static void main(String[] args) {
+        //Incremento e decremento
+        int x = 10;
+        x++;
+        System.out.println(x);
+        x--;
+        System.out.println(x);
+        x--;
+        System.out.println(x);
+
+        //Contador de login
+        Scanner entrada = new Scanner(System.in);
+        int tentativas = 5;
+        System.out.println("Digite o login: ");
+        while (tentativas > 0) {
+            entrada.nextLine();
+            tentativas--;
+            System.out.println("Você ainda tem " + tentativas + " tentativas restando");
+            System.out.println("Digite novamente o login: ");
+        }
+        System.out.println("Você excedeu o numero de tentaivas. Finalizando...");
+
+    }
+}
