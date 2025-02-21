03f2bceb6537fae92b801b42b41cf6d7b6dc0dff
hadesds <hadessds@gmail.com>
2025-02-21 11:14:48 -0300

    Atividade Operadores de Atribuição

---
+++ src/operadoresAtribuicao.java
@@ -0,0 +1,13 @@
+public class operadoresAtribuicao {
+    public static void main(String[] args) {
+        //Incremento
+        int saldo = 500;
+        saldo += 50;
+        System.out.println(saldo);
+
+        //Atribuição de multiplicação
+        double preco = 150.0;
+        preco *= 0.10;
+        System.out.println("Você recebeu R$" + preco + " de desconto.");
+    }
+}
