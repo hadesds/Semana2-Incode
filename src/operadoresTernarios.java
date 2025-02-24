8bd04ec4715a7c6d790a07095c120dacf143d7f8
hadesds <hadessds@gmail.com>
2025-02-24 08:55:04 -0300

    Operadores Ternarios

---
+++ src/operadoresTernarios.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class operadoresTernarios {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe sua idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.println(idade >= 18 ?
+                "Você é maior de idade" : "Você é menor de idade");
+    }
+}
