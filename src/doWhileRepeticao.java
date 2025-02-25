7c2e0e10fdc5f20ef7ac921a6834ecfdee5b4901
hadesds <hadessds@gmail.com>
2025-02-25 09:06:28 -0300

    Do While atividade

---
+++ src/doWhileRepeticao.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class doWhileRepeticao {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        Integer valor = 0;
+
+        do {
+            System.out.println("Quantas vezes quer repetir: ");
+            valor = entrada.nextInt();
+        } while (valor != 0);
+    }
+}
---
+++ src/doWhileSenha.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class doWhileSenha {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        String senha = "hades";
+        do {
+            System.out.println("Digite o login: ");
+            senha = entrada.nextLine();
+        } while (!senha.equalsIgnoreCase("hades"));
+        entrada.close();
+    }}
